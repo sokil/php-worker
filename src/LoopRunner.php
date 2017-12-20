e33b136ea0fd9260749c449d8c8f7c4bfcd3d294
Sokil <dmytro.sokil@gmail.com>
2017-12-20 12:17:57 +0200

    Update LoopRunner.php

--- src/LoopRunner.php
+++ src/LoopRunner.php
@@ -1,1 +1,19 @@
 
+<?php
+declare(strict_types=1);
+
+namespace Sokil\Worker;
+
+class LoopRunner
+{
+    public function __construct() {
+    }
+
+    /**
+     * @param TickInterface $tick
+     */
+    public function run(Loop $loop)
+    {
+
+    }
+}
